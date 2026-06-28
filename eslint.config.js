6ea7248aec85464c7d8a9139f8df73ed4572638e
Elcio Pereira <elcio.pereira@jbq.lobal>
2026-06-28 16:19:35 -0300

    chore: add eslint flat config

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/coverage/**'
+    ]
+  },
+
+  js.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  prettier,
+
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node
+      }
+    },
+    rules: {
+      'no-console': 'off'
+    }
+  }
+);
